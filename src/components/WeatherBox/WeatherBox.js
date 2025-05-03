79c1e7ff8b13048bd7675fe0bf6f0c81f7eae966
Piotr Lecicki <p.lecicki@gmail.com>
2025-05-03 09:57:38 +0200

    Warm up - Stage 2

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,28 +1,30 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
-import {useCallback} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 const WeatherBox = props => {
+
+  const [weatherData, setWeatherData] = useState('')
 
   const handleCityChange = useCallback((cityName) => {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a8c2c741a470f57c6bb8c04742d3c9c8&units=metric`)
       .then(res => res.json())
       .then(data => {
         console.log(data);
-        const weatherData = {
+        setWeatherData({
           city: data.name,
           temp: data.main.temp,
           icon: data.weather[0].icon,
           description: data.weather[0].main
-        };
+        });
       });
   }, [])
 
   return (
     <section>
       <PickCity action={handleCityChange}/>
-      <WeatherSummary />
+      <WeatherSummary weatherData={weatherData}/>
       <Loader />
     </section>
   )
--- src/components/WeatherSummary/WeatherSummary.js
+++ src/components/WeatherSummary/WeatherSummary.js
@@ -1,16 +1,16 @@
 import styles from './WeatherSummary.module.scss';
 
-const WeatherSummary = () => {
+const WeatherSummary = props => {
   return (
     <section className={styles.weatherSummary}>
       <img
         className={styles.weatherIcon}
-        alt="????"
-        src={`${process.env.PUBLIC_URL}/images/weather-icons/13d.png`} />
+        alt={props.weatherData.description}
+        src={`${process.env.PUBLIC_URL}/images/weather-icons/${props.weatherData.icon}.png`} />
       <div className={styles.weatherInfo}>
-        <h2>London</h2>
+        <h2>{props.weatherData.city}</h2>
         <p>
-          <strong>Temp:</strong> 20°C
+          <strong>Temp:</strong> {props.weatherData.temp}°C
         </p>
       </div>
     </section>
